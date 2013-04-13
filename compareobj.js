90f0895e9ee7111c8cf45e29d8e8ada33ec88e46
chris <chris@bumblehead.com>
2013-04-12 23:31:15 -0700

    added new method `isSameMembersDefinedSame`

--- compareobj.js
+++ compareobj.js
@@ -1,5 +1,5 @@
 // Filename: compareobj.js  
-// Timestamp: 2013.04.12-21:36:39 (last modified)  
+// Timestamp: 2013.04.12-23:30:39 (last modified)  
 // Author(s): Bumblehead (www.bumblehead.com)  
 
 var CompareObj = module.exports = (function () {
@@ -33,9 +33,41 @@ var CompareObj = module.exports = (function () {
     }
     return true;
   }
+
+
+  function isSameMembersDefinedSame (obj1, obj2, fn) {
+    var isChildMissing = false, 
+        membersUndefined,
+        isMembersUndefined;
+
+    for (var o in obj1) {
+      if (obj1.hasOwnProperty(o)) {
+        // if value is an empty string... 
+        // we assume property, value is still valid.
+        if (obj2[o] === obj1[o]) {
+          if (typeof obj1[o] === 'object') {
+            membersUndefined = isSameMembersDefinedSame(obj1[o], obj2[o], fn);
+            if (membersUndefined) {
+              isMembersUndefined = true;
+            }
+          }
+          
+          if (isMembersUndefined) {
+            if (typeof fn === 'function') fn('child', o);
+            return false;
+          }
+        } else {
+          if (typeof fn === 'function') fn('property', o);
+          return false;
+        }
+      }
+    }
+    return true;
+  }
   
   return {
-    isSameMembersDefined : isSameMembersDefined
+    isSameMembersDefined : isSameMembersDefined,
+    isSameMembersDefinedSame : isSameMembersDefinedSame
   };
     
 }());
